b147f5ffef3bed46b292eed3a0e1de0f7492ea04
trunghau4869 <83228878+trunghau4869@users.noreply.github.com>
2022-05-04 00:04:46 +0700

    update 4/5
    update 4/5

--- newProject/src/main/java/com/example/demo/service/payPalService/PayPalPaymentIntent.java
+++ newProject/src/main/java/com/example/demo/service/payPalService/PayPalPaymentIntent.java
@@ -1,5 +1,5 @@
-package com.example.demo.service.payPalService;
-
-public enum PayPalPaymentIntent {
-    sale, authorize, order
-}
+//package com.example.demo.service.payPalService;
+//
+//public enum PayPalPaymentIntent {
+//    sale, authorize, order
+//}
